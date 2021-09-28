1fd01f6801cb7d872caa278d1e3d3ffc1ce81b3d
Jevgeni <jevgeni1@hotmail.com>
2021-09-28 18:37:31 +0300

    JavaHello

---
+++ src/sktvp20task1/SKTVp20Task1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sktvp20task1;
+
+/**
+ *
+ * @author jevge
+ */
+public class SKTVp20Task1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("JavaHello");
+    }
+    
+}
